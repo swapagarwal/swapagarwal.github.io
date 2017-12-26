596a008200f660d7fa787c07270ced3fdefa49fe
Swapnil Agarwal <swapnilagarwal@live.com>
2017-12-27 01:42:31 +0530

    Add github activity

---
+++ pages/github.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <iframe allowtransparency="true" frameborder="0" scrolling="no" seamless="seamless"
+          src="https://colmdoyle.github.io/gh-activity/gh-activity.html?user=swapagarwal&type=user" width="300" height="300"></iframe>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
