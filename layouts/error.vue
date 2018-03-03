2401cd1e7dd8739492104a697190b6de19ef9163
Swapnil Agarwal <swapnilagarwal@live.com>
2018-03-03 23:48:19 +0530

    Make error pages great again!

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        404 not found!
+    <div align="center">
+        Go back to the <a href="/">home page</a>.
+        <br>
+        <img src="404.jpg" alt="404 not found!">
     </div>
 </template>
 
