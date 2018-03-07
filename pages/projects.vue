24f26227c48e8874c7356a6a61c189136ba13a36
Swapnil Agarwal <swapnilagarwal@live.com>
2018-03-07 23:06:51 +0530

    /projects: add swag-for-dev and mediumship

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,16 +1,16 @@
 <template>
     <div>
-        <div class="github-card" data-github="swapagarwal/JARVIS-on-Messenger" data-width="400" data-height="203" data-theme="default"></div>
+        <div class="github-card" data-github="swapagarwal/SaveURLtoDropbox" data-width="400" data-height="155" data-theme="default"></div>
+        <div class="github-card" data-github="swapagarwal/swag-for-dev" data-width="400" data-height="155" data-theme="default"></div>
+        <div class="github-card" data-github="swapagarwal/pm" data-width="400" data-height="155" data-theme="default"></div>
+        <br><br>
         <div class="github-card" data-github="swapagarwal/bunnies-and-badgers" data-width="400" data-height="203" data-theme="default"></div>
-        <div class="github-card" data-github="swapagarwal/medium-play" data-width="400" data-height="203" data-theme="default"></div>
+        <div class="github-card" data-github="swapagarwal/JARVIS-on-Messenger" data-width="400" data-height="203" data-theme="default"></div>
+        <div class="github-card" data-github="swapagarwal/mediumship" data-width="400" data-height="203" data-theme="default"></div>
         <br><br>
         <div class="github-card" data-github="swapagarwal/sendgrid-flask-heroku" data-width="400" data-height="179" data-theme="default"></div>
         <div class="github-card" data-github="swapagarwal/awesome-inspiration" data-width="400" data-height="179" data-theme="default"></div>
         <div class="github-card" data-github="swapagarwal/shoutlink" data-width="400" data-height="179" data-theme="default"></div>
-        <br><br>
-        <div class="github-card" data-github="swapagarwal/pm" data-width="400" data-height="155" data-theme="default"></div>
-        <div class="github-card" data-github="swapagarwal/gotapper" data-width="400" data-height="155" data-theme="default"></div>
-        <div class="github-card" data-github="swapagarwal/SaveURLtoDropbox" data-width="400" data-height="155" data-theme="default"></div>
         <br><br>
         <div class="github-card" data-github="swapagarwal/dropbox_ignore" data-width="400" data-height="150" data-theme="default"></div>
         <script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>
