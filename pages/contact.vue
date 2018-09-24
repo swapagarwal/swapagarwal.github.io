0d5539dec9f8a8217673e1d2bcf7c32942713c16
Swapnil Agarwal <swapnilagarwal@live.com>
2018-09-24 08:00:13 +0530

    contact: update text

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,7 +2,7 @@
     <div class="contact">
         You can use the chat widget at the bottom right to send me a message.
         <br><br>
-        I'd love to connect. <a href="https://calendly.com/swapnilagarwal">Here's my calendar link</a> to make finding time easy.
+        I'd love to connect online or over a cup of coffee. <a href="https://calendly.com/swapnilagarwal">Here's my calendar link</a> to make finding time easy.
     </div>
 </template>
 
