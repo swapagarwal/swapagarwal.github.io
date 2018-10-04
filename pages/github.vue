9c8211bf03d3800f19d492d6039314246466dfbe
Swapnil Agarwal <swapnilagarwal@live.com>
2018-10-04 16:14:16 +0530

    github: replace activity component

--- pages/github.vue
+++ pages/github.vue
@@ -1,20 +1,20 @@
 <template>
     <div>
         <center>
-            <div class="github-card" data-github="swapagarwal" data-width="400" data-height="152" data-theme="default"></div>
-            <script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>
+            <github-feed login="swapagarwal"></github-feed>
         </center>
-        <p align="center">
-            <iframe allowtransparency="true" frameborder="0" scrolling="no" seamless="seamless"
-              src="https://colmdoyle.github.io/gh-activity/gh-activity.html?user=swapagarwal&type=user" width="600" height="600"></iframe>
-        </p>
     </div>
 </template>
 
 <script>
-    export default {
+import Vue from 'vue'
+import VueGithubActivity from 'vue-github-activity'
 
-    }
+Vue.use(VueGithubActivity)
+
+export default {
+
+}
 </script>
 
 <style scoped>
