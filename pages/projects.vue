7143c267c8e8a8dde44adcd89701633d7237cb8e
Swapnil Agarwal <swapnilagarwal@live.com>
2019-03-16 06:32:59 +0530

    projects: add header navbar

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,111 +1,118 @@
 <template>
-    <div class="projects">
-        <div style="text-align: center;">
-            <a href="https://github.com/swapagarwal" target="_blank" rel="noopener noreferrer">Follow me on GitHub</a> to see what I'm currently working on!
+    <div>
+        <Header/>
+        <div class="projects">
+            <div style="text-align: center;">
+                <a href="https://github.com/swapagarwal" target="_blank" rel="noopener noreferrer">Follow me on GitHub</a> to see what I'm currently working on!
+                <br><br>
+                <div class="github-card" data-github="swapagarwal" data-width="400" data-height="152" data-theme="default"></div>
+        <script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>
+            </div>
+            <br>
+            Some of my projects include:
             <br><br>
-            <div class="github-card" data-github="swapagarwal" data-width="400" data-height="152" data-theme="default"></div>
-    <script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>
+            <div style="display: flex;">
+                <div class="card">
+                    <div class="container padding">
+                        <gh-btns-star
+                            slug="swapagarwal/swag-for-dev"
+                            show-count
+                        />
+                        <gh-btns-fork
+                            slug="swapagarwal/swag-for-dev"
+                            show-count
+                        />
+                    </div>
+                    <a href="http://devswag.io/" target="_blank" rel="noopener noreferrer">
+                        <img src="https://partners.npmjs.com/weekly/weekly30/socks-1100x.jpg" alt="Avatar" style="width:100%">
+                        <div class="container">
+                            <h4>
+                                <b>swag-for-dev</b>
+                            </h4>
+                            <p>😎 swag opportunities for developers</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="card">
+                    <div class="container padding">
+                        <gh-btns-star
+                            slug="swapagarwal/JARVIS-on-Messenger"
+                            show-count
+                        />
+                        <gh-btns-fork
+                            slug="swapagarwal/JARVIS-on-Messenger"
+                            show-count
+                        />
+                    </div>
+                    <a href="https://github.com/swapagarwal/JARVIS-on-Messenger" target="_blank" rel="noopener noreferrer">
+                        <img src="https://slideplayer.com/slide/8640299/26/images/1/Initializing%E2%80%A6.jpg" alt="Avatar" style="width:100%">
+                        <div class="container">
+                            <h4>
+                                <b>JARVIS-on-Messenger</b>
+                            </h4>
+                            <p>💬 A community-driven python bot that aims to be as simple as possible to serve humans with their everyday tasks</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="card">
+                    <div class="container padding">
+                        <gh-btns-star
+                            slug="swapagarwal/awesome-eli5"
+                            show-count
+                        />
+                        <gh-btns-fork
+                            slug="swapagarwal/awesome-eli5"
+                            show-count
+                        />
+                    </div>
+                    <a href="https://github.com/swapagarwal/awesome-eli5" target="_blank" rel="noopener noreferrer">
+                        <img src="https://howhttps.works/assets/images/how-https-works-open-graph.png" alt="Avatar" style="width:100%">
+                        <div class="container">
+                            <h4>
+                                <b>awesome-eli5</b>
+                            </h4>
+                            <p>👶 Technical concepts explained in layman terms! git.io/eli5</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="card">
+                    <div class="container padding">
+                        <gh-btns-star
+                            slug="swapagarwal/awesome-inspiration"
+                            show-count
+                        />
+                        <gh-btns-fork
+                            slug="swapagarwal/awesome-inspiration"
+                            show-count
+                        />
+                    </div>
+                    <a href="https://github.com/swapagarwal/awesome-inspiration" target="_blank" rel="noopener noreferrer">
+                        <img src="https://callingdreams.com/wp-content/uploads/2016/02/Michael-Jordan-Quote-1.jpg" alt="Avatar" style="width:100%">
+                        <div class="container">
+                            <h4>
+                                <b>awesome-inspiration</b>
+                            </h4>
+                            <p>🔥 A curated list of content worth sharing to help you get inspired! ⚡️</p>
+                        </div>
+                    </a>
+                </div>
+                <div>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                </div>
+            </div>
+            <br><br><br><br>
         </div>
-        <br>
-        Some of my projects include:
-        <br><br>
-        <div style="display: flex;">
-            <div class="card">
-                <div class="container padding">
-                    <gh-btns-star
-                        slug="swapagarwal/swag-for-dev"
-                        show-count
-                    />
-                    <gh-btns-fork
-                        slug="swapagarwal/swag-for-dev"
-                        show-count
-                    />
-                </div>
-                <a href="http://devswag.io/" target="_blank" rel="noopener noreferrer">
-                    <img src="https://partners.npmjs.com/weekly/weekly30/socks-1100x.jpg" alt="Avatar" style="width:100%">
-                    <div class="container">
-                        <h4>
-                            <b>swag-for-dev</b>
-                        </h4>
-                        <p>😎 swag opportunities for developers</p>
-                    </div>
-                </a>
-            </div>
-            <div class="card">
-                <div class="container padding">
-                    <gh-btns-star
-                        slug="swapagarwal/JARVIS-on-Messenger"
-                        show-count
-                    />
-                    <gh-btns-fork
-                        slug="swapagarwal/JARVIS-on-Messenger"
-                        show-count
-                    />
-                </div>
-                <a href="https://github.com/swapagarwal/JARVIS-on-Messenger" target="_blank" rel="noopener noreferrer">
-                    <img src="https://slideplayer.com/slide/8640299/26/images/1/Initializing%E2%80%A6.jpg" alt="Avatar" style="width:100%">
-                    <div class="container">
-                        <h4>
-                            <b>JARVIS-on-Messenger</b>
-                        </h4>
-                        <p>💬 A community-driven python bot that aims to be as simple as possible to serve humans with their everyday tasks</p>
-                    </div>
-                </a>
-            </div>
-            <div class="card">
-                <div class="container padding">
-                    <gh-btns-star
-                        slug="swapagarwal/awesome-eli5"
-                        show-count
-                    />
-                    <gh-btns-fork
-                        slug="swapagarwal/awesome-eli5"
-                        show-count
-                    />
-                </div>
-                <a href="https://github.com/swapagarwal/awesome-eli5" target="_blank" rel="noopener noreferrer">
-                    <img src="https://howhttps.works/assets/images/how-https-works-open-graph.png" alt="Avatar" style="width:100%">
-                    <div class="container">
-                        <h4>
-                            <b>awesome-eli5</b>
-                        </h4>
-                        <p>👶 Technical concepts explained in layman terms! git.io/eli5</p>
-                    </div>
-                </a>
-            </div>
-            <div class="card">
-                <div class="container padding">
-                    <gh-btns-star
-                        slug="swapagarwal/awesome-inspiration"
-                        show-count
-                    />
-                    <gh-btns-fork
-                        slug="swapagarwal/awesome-inspiration"
-                        show-count
-                    />
-                </div>
-                <a href="https://github.com/swapagarwal/awesome-inspiration" target="_blank" rel="noopener noreferrer">
-                    <img src="https://callingdreams.com/wp-content/uploads/2016/02/Michael-Jordan-Quote-1.jpg" alt="Avatar" style="width:100%">
-                    <div class="container">
-                        <h4>
-                            <b>awesome-inspiration</b>
-                        </h4>
-                        <p>🔥 A curated list of content worth sharing to help you get inspired! ⚡️</p>
-                    </div>
-                </a>
-            </div>
-            <div>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            </div>
-        </div>
-        <br><br><br><br>
     </div>
 </template>
 
 <script>
-    export default {
+import Header from '~/components/Header.vue'
 
-    }
+export default {
+  components: {
+    Header
+  }
+}
 </script>
 
 <style scoped>
