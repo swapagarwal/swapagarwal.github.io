60665f52b99b05183ed8112e6597798eff72ee5f
Swapnil Agarwal <swapnilagarwal@live.com>
2019-03-17 06:33:21 +0530

    ideas: add header navbar

--- pages/ideas.vue
+++ pages/ideas.vue
@@ -1,27 +1,34 @@
 <template>
-    <div class="ideas">
-        <h2>I 🧞 wish there existed:</h2>
-        <ul>
-            <li>
-                A chrome extension to limit infinite scroll on social media to reduce content addiction!
-            </li>
-            <li>
-                A gym that increases your monthly fee based on how frequently you go.
-            </li>
-            <li>
-                Cosplay for hospitals; we need more of that!
-            </li>
-            <li>
-                Generic caching proxy as a service (for using as a wrapper over rate-limited APIs).
-            </li>
-        </ul>
-        Let me know if you end up making one of these / know that it exists already. I'll be quite thankful! 🙏
+    <div>
+        <Header/>
+        <div class="ideas">
+            <h2>I 🧞 wish there existed:</h2>
+            <ul>
+                <li>
+                    A chrome extension to limit infinite scroll on social media to reduce content addiction!
+                </li>
+                <li>
+                    A gym that increases your monthly fee based on how frequently you go.
+                </li>
+                <li>
+                    Cosplay for hospitals; we need more of that!
+                </li>
+                <li>
+                    Generic caching proxy as a service (for using as a wrapper over rate-limited APIs).
+                </li>
+            </ul>
+            Let me know if you end up making one of these / know that it exists already. I'll be quite thankful! 🙏
+        </div>
     </div>
 </template>
 
 <script>
-export default {
+import Header from '~/components/Header.vue'
 
+export default {
+  components: {
+    Header
+  }
 }
 </script>
 
