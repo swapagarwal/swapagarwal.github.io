abc2af88d58e7e7386ae99fc69f37ce4309c0712
Swapnil Agarwal <swapnilagarwal@live.com>
2019-04-05 07:14:38 +0530

    uses: add header navbar

--- pages/uses.vue
+++ pages/uses.vue
@@ -1,61 +1,68 @@
 <template>
-    <div class="uses">
-        <h2>🦄 Things I use:</h2>
-        <ul>
-            <li>
-                ⚔️ <a href="https://todoist.com/r/swapnilagarwal1994_powvaa" target="_blank" rel="noopener noreferrer">Todoist</a> for task management <a href="https://buttondown.email/swap/archive/21073491-4637-4235-a9e7-a9ebe6a21f5f" target="_blank" rel="noopener noreferrer">Read more...</a>
-            <li>
-                🌐 <a href="https://www.namecheap.com/" target="_blank" rel="noopener noreferrer">Namecheap</a> for registering domains
-            </li>
-            <li>
-                🏃‍♂️ <a href="https://airtable.com/invite/r/3vcHWIIt" target="_blank" rel="noopener noreferrer">Airtable</a> for spinning up a quick database
-            </li>
-            <li>
-                ✍️ <a href="https://www.notion.so/?r=ee9dea5d2e09438480bb97aa7a338770" target="_blank" rel="noopener noreferrer">Notion</a> for documentation
-            </li>
-            <li>
-                👨‍💻 <a href="https://code.visualstudio.com/" target="_blank" rel="noopener noreferrer">Visual Studio Code</a> as my primary editor
-            </li>
-            <li>
-                ⬛ <a href="https://www.iterm2.com/" target="_blank" rel="noopener noreferrer">iTerm2</a> as my terminal (with <a href="https://github.com/robbyrussell/oh-my-zsh" target="_blank" rel="noopener noreferrer">oh-my-zsh</a> framework)
-            </li>
-            <li>
-                🌙 <a href="https://justgetflux.com/" target="_blank" rel="noopener noreferrer">f.lux</a> to make life better
-            </li>
-            <li>
-                🗒️ <a href="http://www.clipmenu.com/" target="_blank" rel="noopener noreferrer">ClipMenu</a> as my clipboard manager
-            </li>
-            <li>
-                ☕ <a href="https://itunes.apple.com/us/app/amphetamine/id937984704?mt=12" target="_blank" rel="noopener noreferrer">Amphetamine</a> for keeping mac awake
-            </li>
-            <li>
-                🚀 <a href="https://matthewpalmer.net/rocket/" target="_blank" rel="noopener noreferrer">Rocket</a> to keep emojis at my fingertips
-            </li>
-            <li>
-                🔑 <a href="https://myki.com/" target="_blank" rel="noopener noreferrer">Myki</a> for managing passwords w/o a master password <a href="https://buttondown.email/swap/archive/4c1a6e0a-4492-45e8-a0b5-2e47eae060f2" target="_blank" rel="noopener noreferrer">Read more...</a>
-            </li>
-            <li>
-                💭 <a href="https://dayoneapp.com/" target="_blank" rel="noopener noreferrer">Day One</a> for capturing moments <a href="https://buttondown.email/swap/archive/5d7b889b-3ef6-4861-9a5b-671cd668322b" target="_blank" rel="noopener noreferrer">Read more...</a>
-            </li>
-            <li>
-                🗃️ <a href="https://db.tt/OYuASVR3HM" target="_blank" rel="noopener noreferrer">Dropbox</a> for backup purposes
-            </li>
-            <li>
-                🎵 <a href="https://www.spotify.com/" target="_blank" rel="noopener noreferrer">Spotify</a> for its awesome dose of discover weekly
-            </li>
-            <li>
-                🦊 <a href="https://www.mozilla.org/en-US/firefox/mobile/" target="_blank" rel="noopener noreferrer">Firefox Focus</a> for mobile browsing
-            </li>
-            <li>
-                📌 <a href="https://www.pushbullet.com/" target="_blank" rel="noopener noreferrer">PushBullet</a> to share stuff b/w devices
-            </li>
-        </ul>
+    <div>
+        <Header/>
+        <div class="uses">
+            <h2>🦄 Things I use:</h2>
+            <ul>
+                <li>
+                    ⚔️ <a href="https://todoist.com/r/swapnilagarwal1994_powvaa" target="_blank" rel="noopener noreferrer">Todoist</a> for task management <a href="https://buttondown.email/swap/archive/21073491-4637-4235-a9e7-a9ebe6a21f5f" target="_blank" rel="noopener noreferrer">Read more...</a>
+                <li>
+                    🌐 <a href="https://www.namecheap.com/" target="_blank" rel="noopener noreferrer">Namecheap</a> for registering domains
+                </li>
+                <li>
+                    🏃‍♂️ <a href="https://airtable.com/invite/r/3vcHWIIt" target="_blank" rel="noopener noreferrer">Airtable</a> for spinning up a quick database
+                </li>
+                <li>
+                    ✍️ <a href="https://www.notion.so/?r=ee9dea5d2e09438480bb97aa7a338770" target="_blank" rel="noopener noreferrer">Notion</a> for documentation
+                </li>
+                <li>
+                    👨‍💻 <a href="https://code.visualstudio.com/" target="_blank" rel="noopener noreferrer">Visual Studio Code</a> as my primary editor
+                </li>
+                <li>
+                    ⬛ <a href="https://www.iterm2.com/" target="_blank" rel="noopener noreferrer">iTerm2</a> as my terminal (with <a href="https://github.com/robbyrussell/oh-my-zsh" target="_blank" rel="noopener noreferrer">oh-my-zsh</a> framework)
+                </li>
+                <li>
+                    🌙 <a href="https://justgetflux.com/" target="_blank" rel="noopener noreferrer">f.lux</a> to make life better
+                </li>
+                <li>
+                    🗒️ <a href="http://www.clipmenu.com/" target="_blank" rel="noopener noreferrer">ClipMenu</a> as my clipboard manager
+                </li>
+                <li>
+                    ☕ <a href="https://itunes.apple.com/us/app/amphetamine/id937984704?mt=12" target="_blank" rel="noopener noreferrer">Amphetamine</a> for keeping mac awake
+                </li>
+                <li>
+                    🚀 <a href="https://matthewpalmer.net/rocket/" target="_blank" rel="noopener noreferrer">Rocket</a> to keep emojis at my fingertips
+                </li>
+                <li>
+                    🔑 <a href="https://myki.com/" target="_blank" rel="noopener noreferrer">Myki</a> for managing passwords w/o a master password <a href="https://buttondown.email/swap/archive/4c1a6e0a-4492-45e8-a0b5-2e47eae060f2" target="_blank" rel="noopener noreferrer">Read more...</a>
+                </li>
+                <li>
+                    💭 <a href="https://dayoneapp.com/" target="_blank" rel="noopener noreferrer">Day One</a> for capturing moments <a href="https://buttondown.email/swap/archive/5d7b889b-3ef6-4861-9a5b-671cd668322b" target="_blank" rel="noopener noreferrer">Read more...</a>
+                </li>
+                <li>
+                    🗃️ <a href="https://db.tt/OYuASVR3HM" target="_blank" rel="noopener noreferrer">Dropbox</a> for backup purposes
+                </li>
+                <li>
+                    🎵 <a href="https://www.spotify.com/" target="_blank" rel="noopener noreferrer">Spotify</a> for its awesome dose of discover weekly
+                </li>
+                <li>
+                    🦊 <a href="https://www.mozilla.org/en-US/firefox/mobile/" target="_blank" rel="noopener noreferrer">Firefox Focus</a> for mobile browsing
+                </li>
+                <li>
+                    📌 <a href="https://www.pushbullet.com/" target="_blank" rel="noopener noreferrer">PushBullet</a> to share stuff b/w devices
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
+import Header from '~/components/Header.vue'
 
+export default {
+  components: {
+    Header
+  }
 }
 </script>
 
