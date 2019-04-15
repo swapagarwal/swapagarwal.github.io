c37b8e5161ed8e29c562e55f0bb41ab64c876083
Swapnil Agarwal <swapnilagarwal@live.com>
2019-04-15 22:20:21 +0530

    index: update newsletter's latest post

--- pages/index.vue
+++ pages/index.vue
@@ -47,7 +47,7 @@
           <input type="hidden" value="1" name="embed"/>
           <input type="submit" value="subscribe" style="cursor: pointer; margin-left: -112px; border-radius: 10px; background-color: #0069FF; color: #fff; border: none; font-size: 18px;"/>
         </form>
-        🔥 Latest Post: <a href="https://buttondown.email/swap/archive/21073491-4637-4235-a9e7-a9ebe6a21f5f" target="_blank" rel="noopener noreferrer">#18: ⚔️ A Personal Assistant to Maximize your Productivity</a>
+        🔥 Latest Post: <a href="https://buttondown.email/swap/archive/book-notes-factfulness-ten-reasons-were-wrong/" target="_blank" rel="noopener noreferrer">#19: 📚 Book Notes: Factfulness: Ten Reasons We're Wrong About the World - and Why Things Are Better Than You Think</a>
       </div>
       <br>
       <div class="contact">
