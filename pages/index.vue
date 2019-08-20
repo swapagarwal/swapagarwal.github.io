236d222e0c8775c489d3a884c3b655fbd2d94ca2
Swapnil Agarwal <hi+github@swapnil.net>
2019-08-20 18:44:14 +0530

    index: update newsletter's latest post

--- pages/index.vue
+++ pages/index.vue
@@ -78,7 +78,7 @@
           <input type="hidden" value="1" name="embed"/>
           <input type="submit" value="subscribe" style="cursor: pointer; margin-left: -112px; border-radius: 10px; background-color: #0069FF; color: #fff; border: none; font-size: 18px;"/>
         </form>
-        🔥 Latest Post: <a href="https://buttondown.email/swap/archive/book-notes-the-art-of-war/" target="_blank" rel="noopener noreferrer">#35: 📚 Book Notes: The Art of War</a>
+        🔥 Latest Post: <a href="https://buttondown.email/swap/archive/fighting-the-imposter-syndrome/" target="_blank" rel="noopener noreferrer">#36: 🧞 Fighting the Imposter Syndrome</a>
       </div>
       <br>
       <div class="contact">
