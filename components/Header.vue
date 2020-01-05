08d424db0e594b9adfb79fe3d16af0294aaa5bda
Swapnil Agarwal <hi+github@swapnil.net>
2020-01-05 14:04:54 +0530

    header: hide ideas

--- components/Header.vue
+++ components/Header.vue
@@ -3,7 +3,7 @@
     <nuxt-link to="/" exact>&nbsp;🏡 Home&nbsp;</nuxt-link>
     <!-- <nuxt-link to="/projects">&nbsp;📂 Projects&nbsp;</nuxt-link> -->
     <nuxt-link to="/uses">&nbsp;🎁 Uses&nbsp;</nuxt-link>
-    <nuxt-link to="/ideas">&nbsp;🧠 Ideas&nbsp;</nuxt-link>
+    <!-- <nuxt-link to="/ideas">&nbsp;🧠 Ideas&nbsp;</nuxt-link> -->
     <nuxt-link to="/faq">&nbsp;❓ F.A.Q.&nbsp;</nuxt-link>
   </div>
 </template>
