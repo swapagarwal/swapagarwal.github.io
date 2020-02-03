4cd3e066ff438274bb4f17ffff8c668220d8f70a
Swapnil Agarwal <hi+github@swapnil.net>
2020-02-03 23:29:00 +0530

    reads: update review

--- pages/reads.vue
+++ pages/reads.vue
@@ -56,7 +56,7 @@ import Footer from '~/components/Footer.vue'
             author: 'Nir Eyal',
             link: 'https://amzn.to/2SwX3tr',
             image: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1559847987l/44595007._SX98_.jpg',
-            review: `I liked part 6 - raising indistractable children. 🤞<br />Rest was skippable - I’ve already learnt these over the years; would’ve been super helpful 5 years ago!`,
+            review: `I liked part 6 - raising indistractable children.<br />Rest was skippable - I’ve already learnt these over the years; would’ve been super helpful 5 years ago!`,
             rating: '2 of 5 stars',
             notes: 'https://medium.com/@swap/book-notes-indistractable-7af2f832a3a6'
           },
