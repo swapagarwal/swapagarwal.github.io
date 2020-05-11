73e206d28f8366a12dfaefb52b77ffe84f11a412
Swapnil Agarwal <hi+github@swapnil.net>
2020-05-11 11:41:05 +0530

    404: fix image path

--- layouts/error.vue
+++ layouts/error.vue
@@ -2,7 +2,7 @@
     <div align="center">
         Go back to the <a href="/">home page</a>.
         <br>
-        <img src="404.jpg" alt="404 not found!">
+        <img src="/404.jpg" alt="404 not found!">
     </div>
 </template>
 
