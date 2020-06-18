4b259dda198f3926ac068a837c5d837d67364b39
Swapnil Agarwal <hi+github@swapnil.net>
2020-06-18 18:32:13 +0530

    faq: update services

--- pages/faq.vue
+++ pages/faq.vue
@@ -132,7 +132,6 @@ export default {
                 <li class="list-item"><a href="https://www.buymeacoffee.com/l/cvraas" target="_blank" rel="noopener noreferrer">Review my CV</a></li>
                 <li class="list-item">Mentoring session</li>
                 <li class="list-item"><a href="/fund">Fund</a></li>
-                <li class="list-item">I'm currently accepting business consulting contracts.</li>
             </ul>`,
           category: 'Recommendations'
         }
