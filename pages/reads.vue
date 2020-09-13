2d65a1cff25a088ebbd37e5deb00f405c74c1b4c
Swapnil Agarwal <hi+github@swapnil.net>
2020-09-13 16:41:17 +0530

    reads: add meta tags

--- pages/reads.vue
+++ pages/reads.vue
@@ -44,7 +44,11 @@ import Footer from '~/components/Footer.vue'
       return {
         title: this.title,
         meta: [
-          { hid: 'description', name: 'description', content: this.description }
+          { hid: 'description', name: 'description', content: this.description },
+          { hid: 'og:title', property: 'og:title', content: this.title },
+          { hid: 'og:description', property: 'og:description', content: this.description },
+          { hid: 'twitter:title', name: 'twitter:title', content: this.title },
+          { hid: 'twitter:description', name: 'twitter:description', content: this.description },
         ]
       }
     },
