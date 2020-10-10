7788e0ffd21ea3343eb8c1ccc301de1af33e6b5c
Swapnil Agarwal <hi+github@swapnil.net>
2020-10-10 19:34:48 +0530

    index: update copy

--- pages/index.vue
+++ pages/index.vue
@@ -18,9 +18,10 @@
       </h2>
       <div>
         I'm currently fixing tech hiring at <a href="https://elevatelabs.tech/" target="_blank" rel="noopener noreferrer">Elevate Labs</a>.
+        I also run a private community of mastermind groups at <a href="https://dxclub.io/" target="_blank" rel="noopener noreferrer">The DX Club</a>.
         <br>
-        I am a Co-Organizer of <a href="https://twitter.com/AnAwesomeConf" target="_blank" rel="noopener noreferrer">💯 Awesome Conference</a> and <a href="https://dxclub.io/taboo" target="_blank" rel="noopener noreferrer">🙅 Screw the Taboo meetups</a>.
-        <br>
+        <!-- I am a Co-Organizer of <a href="https://twitter.com/AnAwesomeConf" target="_blank" rel="noopener noreferrer">💯 Awesome Conference</a> and <a href="https://dxclub.io/taboo" target="_blank" rel="noopener noreferrer">🙅 Screw the Taboo meetups</a>.
+        <br> -->
         In the past, I have worked as a <b>Software Developer at Amazon</b> and as a <b>Product Manager at Meesho</b>.
         <br>
         <!-- I run <a href="http://devswag.io/" target="_blank" rel="noopener noreferrer">😎 devSwag</a> on the side. Some of you might know me as <u>That Swag Guy</u>!
