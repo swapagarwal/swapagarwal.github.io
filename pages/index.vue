2823ba9c76001a9dbc3987cc4b0e612b37e01815
Swapnil Agarwal <hi+github@swapnil.net>
2020-11-13 11:21:06 +0530

    index: testing event tracking

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@
         ></vue-typer>
       </h2>
       <div>
-        I'm fixing tech hiring at <a href="https://elevatelabs.tech/" target="_blank" rel="noopener noreferrer">🚀 Elevate Labs</a>.
+        I'm fixing tech hiring at <a @click="track" href="https://elevatelabs.tech/" target="_blank" rel="noopener noreferrer">🚀 Elevate Labs</a>.
         I also run a private community of mastermind groups at <a href="https://dxclub.io/" target="_blank" rel="noopener noreferrer">⚡️ The DX Club</a>.
         <br>
         <!-- I am a Co-Organizer of <a href="https://twitter.com/AnAwesomeConf" target="_blank" rel="noopener noreferrer">💯 Awesome Conference</a> and <a href="https://dxclub.io/taboo" target="_blank" rel="noopener noreferrer">🙅 Screw the Taboo meetups</a>.
@@ -98,6 +98,11 @@ export default {
     Logo,
     Header,
     Footer
+  },
+  methods: {
+    track: function(event) {
+      this.$ga.event('Home Page', 'Clicked Link', event.target.href);
+    }
   }
 }
 </script>
